7017626637f352c1bd4a8323b5c2020773306ad4
James Trent <jamestrent@Jamess-MacBook-Air.local>
2021-06-14 18:13:41 -0400

    added create account page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppFormComponent } from './app-form/app-form.component';
 import { AppComponent } from './app.component';
+import { CreateAccountPageComponent } from './create-account-page/create-account-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { OverviewComponent } from './overview/overview.component';
 import { HomeComponent } from './home/home.component';
@@ -10,7 +11,7 @@ import { TestStepperComponent } from './test-stepper/test-stepper.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent }, // A1
-  // A2
+  { path: 'create-account-page', component: CreateAccountPageComponent }, // A2
   { path: 'login-page', component: LoginPageComponent }, // A3
   { path: 'overview', component: OverviewComponent }, // A4
   { path: 'home-page', component: AppComponent },

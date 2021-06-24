c693be5127a8b91e3c6b631468960b3f228783ea
Oludare96 <ooo6hy@virginia.edu>
2021-06-24 03:12:54 -0400

    ng build fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    apiKey: "AIzaSyCVPnpztNqu-bWrlkj2Z9iYe21-Qv0kckU",
+    authDomain: "ztsforms.firebaseapp.com",
+    projectId: "ztsforms",
+    storageBucket: "ztsforms.appspot.com",
+    messagingSenderId: "908581137117",
+    appId: "1:908581137117:web:814a9915279e2d5f7b4934",
+    measurementId: "G-DQHP24RW7G"
+  }
 };
